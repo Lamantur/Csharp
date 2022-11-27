3908e23c16564963b4340a508c627b76e3db9e01
lamantur <Lamantur2017@gmail.com>
2022-11-27 13:43:41 +0300

    1 commit

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Prompt(string message)
+{
+    System.Console.WriteLine($"{message} >");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Second(int number)
+{
+    if (number / 1000 == 0 && number / 100 > 0)
+    {
+        int Result;
+        Result = (number - (number / 100) * 100 - number % 10) / 10;
+        return Result;
+    }
+    else
+    {
+        System.Console.WriteLine("ERROR: Введите трехзначное число, согласно условию");
+        return 0;
+    }
+
+}
+
+System.Console.WriteLine(Second(Prompt("Введите число: ")));
