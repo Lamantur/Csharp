92e640816f197af5e5bb2e7ae8caa28bab27c5d3
lamantur <Lamantur2017@gmail.com>
2022-11-27 15:05:25 +0300

    2 commit

--- 2/Program.cs
+++ 2/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Prompt(string message)
+{
+    System.Console.WriteLine($"{message} >");
+    return Convert.ToInt32(Console.ReadLine());
+}
+// Используем метод из задачи 1 для поиска второго числа
+int Second(int number)
+{
+    if (number / 1000 == 0 && number / 100 > 0)
+    {
+        int Result;
+        Result = (number - (number / 100) * 100 - number % 10) / 10;
+        return Result;
+    }
+    else
+    {
+        System.Console.WriteLine("ERROR: Введите трехзначное число, согласно условию");
+        return 0;
+    }
+
+}
+
+int Number = Prompt("Введите число");
+int Result = (Number - (Second(Number) * 10) + (Number % 10) * 10 - Number % 10) / 10;
+System.Console.WriteLine(Result);
